0649cd52d393340ccdacb7f202d77b44ad9d0e05
Karina Kozarova <karinakozarova1303@gmail.com>
2018-01-12 08:58:38 +0200

    Update button.ino

--- button.ino
+++ button.ino
@@ -1,11 +1,9 @@
 void setup() {
-  // put your setup code here, to run once:
   pinMode(8,INPUT_PULLUP); // logic for button click
   pinMode(13,OUTPUT); // diode that lights up
 }
 
 void loop() {
-  // put ryour main code here, to run repeatedly:
   int info =  digitalRead(8); // reads button state
   digitalWrite(13,!info); // writes diode if is pressed
 }
