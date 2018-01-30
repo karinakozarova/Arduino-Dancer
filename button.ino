f00f03be80b6791bb162f1563db22c233a589b1b
Karina Kozarova <karinakozarova1303@gmail.com>
2018-01-30 15:21:16 +0200

    added directions

--- button.ino
+++ button.ino
@@ -1,9 +1,27 @@
 void setup() {
   pinMode(8,INPUT_PULLUP); // logic for button click
+  pinMode(9,INPUT_PULLUP); // logic for button click
+  pinMode(10,INPUT_PULLUP); // logic for button click
+  pinMode(11,INPUT_PULLUP); // logic for button click
+
   pinMode(13,OUTPUT); // diode that lights up
+  Serial.begin(9600);
 }
 
 void loop() {
-  int info =  digitalRead(8); // reads button state
-  digitalWrite(13,!info); // writes diode if is pressed
+  int left =  digitalRead(8); // reads button state
+  digitalWrite(13,!left); // writes diode if is pressed
+  if(!left)Serial.println("left"); // was clicked
+
+  int right =  digitalRead(9); // reads button state
+  digitalWrite(13,!right); // writes diode if is pressed
+  if(!right)Serial.println("right"); // was clicked
+
+  int up =  digitalRead(10); // reads button state
+  digitalWrite(13,!up); // writes diode if is pressed
+  if(!up)Serial.println("up"); // was clicked
+
+  int down =  digitalRead(11); // reads button state
+  digitalWrite(13,!down); // writes diode if is pressed
+  if(!down)Serial.println("down"); // was clicked
 }
