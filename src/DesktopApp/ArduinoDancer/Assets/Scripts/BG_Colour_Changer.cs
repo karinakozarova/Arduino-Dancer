9ddd8ed6a4a288cc98e71d437817584f9899eb63
Magi Marinova <magi.marinowax@gmail.com>
2018-03-21 11:04:32 +0200

    changing background color

---
+++ src/DesktopApp/ArduinoDancer/Assets/Scripts/BG_Colour_Changer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BG_Colour_Changer : MonoBehaviour {
+
+    private Camera camera;
+    //private Color nextCol;
+    private float speed = 0.5f;
+
+    private const float lerpLimit = 0.025f;
+
+	// Use this for initialization
+	void Start () {
+        camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HSBColor randCol = new HSBColor(Mathf.PingPong(Time.time * speed, 1), 1, 1);
+        randCol.s = randCol.s / 2;
+        camera.backgroundColor = randCol.ToColor();
+    }
+}
