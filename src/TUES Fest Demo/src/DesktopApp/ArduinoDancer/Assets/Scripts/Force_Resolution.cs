726887593bedc5a2d301275e2b49820d975fca0a
Karina Kozarova <karinakozarova1303@gmail.com>
2018-04-28 09:18:41 +0300

    added demo

---
+++ src/TUES Fest Demo/src/DesktopApp/ArduinoDancer/Assets/Scripts/Force_Resolution.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Force_Resolution : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        Screen.SetResolution(1920, 1080, false);
+	}
+}
