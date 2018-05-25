ddcf568863439572e12f4bca88a5841595dce867
Karina Kozarova <karinakozarova1303@gmail.com>
2018-05-25 18:58:57 +0300

    dont restart background audio between scenes

---
+++ src/DesktopApp/ArduinoDancer/Assets/Scripts/BG_Audio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_Audio : MonoBehaviour {
+    private static BG_Audio instance = null;
+    public static BG_Audio Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
