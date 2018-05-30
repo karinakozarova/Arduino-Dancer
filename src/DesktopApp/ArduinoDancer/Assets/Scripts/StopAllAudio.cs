b94e9616a5cb17278005ff1ebcf58652794bd7e0
Karina Kozarova <karinakozarova1303@gmail.com>
2018-05-30 10:39:50 +0300

    stop all background music from playing when song starts

---
+++ src/DesktopApp/ArduinoDancer/Assets/Scripts/StopAllAudio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopAllAudio : MonoBehaviour {
+    //Stop all sounds
+    private AudioSource[] allAudioSources;
+
+	// Use this for initialization
+	void Start () {
+        allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        foreach (AudioSource audioS in allAudioSources)
+        {
+            audioS.Stop();
+        }
+	}
+}
